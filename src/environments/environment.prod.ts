4fed379bc1ee89c90aa0dd181096cb9e0e648ba7
samueljb <samueljb@gmail.com>
2019-06-08 23:53:42 -0300

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBUrm8iwgwDNkPlJHcWURutJdOpXqHdyfA',
+    authDomain: 'todolist-62a58.firebaseapp.com',
+    databaseURL: 'https://todolist-62a58.firebaseio.com',
+    projectId: 'todolist-62a58',
+    storageBucket: 'todolist-62a58.appspot.com',
+    messagingSenderId: '834956103192',
+    appId: '1:834956103192:web:0fd485b48d06d774'
+  },
+  serverGCF: {
+    Url: 'https://us-central1-todolist-62a58.cloudfunctions.net/',
+    sendIdToServer: 'https://us-central1-todolist-62a58.cloudfunctions.net/helloWorld'
+  }
+};
